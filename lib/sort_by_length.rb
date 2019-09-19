cd14919322fdcf459bd863a8d0260640e14d2050
Ga-Young Jin <gyjinn@gmail.com>
2019-09-19 11:53:27 -0700

    completed sort by length method

--- lib/sort_by_length.rb
+++ lib/sort_by_length.rb
@@ -3,5 +3,20 @@
 # Time complexity: ?
 # Space complexity: ?
 def sort_by_length(my_sentence)
-  raise NotImplementedError, "Method not implemented"
+  # using bubble sort
+  sentence_in_array = my_sentence.split(" ")
+  i = 0
+  while i < sentence_in_array.length-1
+    j = 0
+    while j < sentence_in_array.length-i-1
+      if sentence_in_array[j].length > sentence_in_array[j+1].length
+        temp = sentence_in_array[j]
+        sentence_in_array[j] = sentence_in_array[j+1]
+        sentence_in_array[j+1] = temp
+      end
+      j += 1
+    end
+    i += 1
+  end
+  return sentence_in_array
 end
