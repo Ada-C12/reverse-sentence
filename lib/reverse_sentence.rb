9a75a0f80a7523390fb7768ed58e7eddcf1a26d9
gracexinran <gracexinran@gmail.com>
2019-09-19 14:43:57 -0700

    completed

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -1,6 +1,45 @@
 # A method to reverse the words in a sentence, in place.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n2)
+# Space complexity: O(1)
+require 'pry'
 def reverse_sentence(my_sentence)
-  raise NotImplementedError
+  # raise NotImplementedError
+  return nil if my_sentence.nil?
+  i = 0
+  j = my_sentence.length - 1
+  until i >= j
+    temp = my_sentence[i]
+    my_sentence[i] = my_sentence[j]
+    my_sentence[j] = temp 
+    i += 1
+    j -= 1
+  end
+  m = 0
+  start_index = m
+  while m < my_sentence.length 
+    # binding.pry
+    if my_sentence[start_index] != " " && my_sentence[m] == " " 
+      my_sentence[start_index..m-1] = reverse_helper(my_sentence[start_index..m-1])
+      start_index = m
+    elsif my_sentence[start_index] != " " && m == my_sentence.length - 1
+      my_sentence[start_index..m] = reverse_helper(my_sentence[start_index..m])
+    elsif my_sentence[start_index] == " " && my_sentence[m] != " "
+      start_index = m
+    end
+    m += 1
+  end
+  return my_sentence
+end
+
+def reverse_helper(my_sentence)
+  i = 0
+  j = my_sentence.length - 1
+  until i >= j
+    temp = my_sentence[i]
+    my_sentence[i] = my_sentence[j]
+    my_sentence[j] = temp 
+    i += 1
+    j -= 1
+  end
+  return my_sentence
 end
--- lib/sort_by_length.rb
+++ lib/sort_by_length.rb
@@ -1,7 +1,23 @@
 # A method which will return an array of the words in the string
 #  sorted by the length of the word.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n2)
+# Space complexity: O(n)
 def sort_by_length(my_sentence)
-  raise NotImplementedError, "Method not implemented"
+  # raise NotImplementedError, "Method not implemented"
+  return nil if my_sentence.nil?
+  i = 0
+  arr = my_sentence.split(' ')
+  while i < arr.length
+    min_length_index = i
+    j = i
+    while j < arr.length
+      min_length_index = j if arr[j].length < arr[min_length_index].length
+      j += 1
+    end
+    temp = arr[i]
+    arr[i] = arr[min_length_index]
+    arr[min_length_index] = temp
+    i += 1  
+  end
+  return arr
 end
