b5b4feb46950390d9aa118443e7011697c53d0e7
Eve Le <nguyenthao.lee@gmail.com>
2019-09-21 00:21:54 -0700

    Implement sort_by_length and reserve_sentence

--- lib/sort_by_length.rb
+++ lib/sort_by_length.rb
@@ -1,7 +1,28 @@
 # A method which will return an array of the words in the string
 #  sorted by the length of the word.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n2) because I use bubble sort for sorting words by their length
+# Space complexity: O(n) because I create array output which contains n number of words in input string
 def sort_by_length(my_sentence)
-  raise NotImplementedError, "Method not implemented"
+  raise ArgumentError if !my_sentence
+  # I'm not sure if I can use .split() here. I can write my own split method
+  # if .split() is not allowed here
+  output = my_sentence.split(" ")
+  length = output.length()
+
+  if length > 0
+    (0...length).each do |i|
+      (0...length - i - 1).each do |j|
+        first = output[j]
+        second = output[j + 1]
+
+        if first.length() > second.length
+          output[j + 1] = first
+          output[j] = second
+        end
+
+      end
+    end
+  end
+  
+  return output
 end
