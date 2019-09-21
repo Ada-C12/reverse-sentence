c0bd036714b4a3f0ca334ff445fc4e61ec5e22fb
Hallie <idhallie@gmail.com>
2019-09-21 13:13:13 -0700

    Passed tests on sort_by_length, working solution on reverse_sentence.

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -1,6 +1,45 @@
 # A method to reverse the words in a sentence, in place.
 # Time complexity: ?
 # Space complexity: ?
+require 'pry'
+
 def reverse_sentence(my_sentence)
-  raise NotImplementedError
+  low = 0
+  high = my_sentence.length - 1
+  
+  while low < high
+    temp = my_sentence[high]
+    my_sentence[high] = my_sentence[low]
+    my_sentence[low] = temp
+    low += 1
+    high -= 1
+  end
+  
+  return my_sentence
 end
+
+my_sentence = reverse_sentence("I love Primo")
+
+word_start = 0
+word_end = -1
+reversed = false
+
+my_sentence.each_char do |char|
+  if char != " " && reversed == false
+    word_end = word_end + 1
+  elsif char == " " && reversed == false
+    my_sentence[word_start..word_end] = reverse_sentence(my_sentence[word_start..word_end])
+    word_end = word_end + 1
+    reversed = true
+  elsif char == " " && reversed == true
+    word_end = word_end + 1
+  elsif char != " " && reversed == true
+    word_end = word_end + 1
+    word_start = word_end
+    reversed = false
+  end
+end
+
+
+
+p my_sentence
--- lib/sort_by_length.rb
+++ lib/sort_by_length.rb
@@ -3,5 +3,24 @@
 # Time complexity: ?
 # Space complexity: ?
 def sort_by_length(my_sentence)
-  raise NotImplementedError, "Method not implemented"
+  array = my_sentence.split
+  length = array.length
+  
+  i = 0
+  while i < length-1
+    j = 0
+    while j < length-i-1
+      if array[j].length > array[j+1].length
+        temp = array[j]
+        array[j] = array[j+1]
+        array[j+1] = temp
+      end
+      j += 1
+    end
+    i += 1
+  end
+  return array
 end
+
+p sort_by_length("I love Ada")
+
