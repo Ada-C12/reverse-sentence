00e41c8671d44e5e11bfd6ad5ca0e8096b188602
Diana Han <dianah46@gmail.com>
2019-09-21 16:46:37 -0700

    passed unit tests for #sort by length method

--- lib/sort_by_length.rb
+++ lib/sort_by_length.rb
@@ -1,7 +1,25 @@
 # A method which will return an array of the words in the string
 #  sorted by the length of the word.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n^2)
+# Space complexity: O(n)
+
 def sort_by_length(my_sentence)
-  raise NotImplementedError, "Method not implemented"
+  array = my_sentence.split
+  length = array.length
+  i = 0
+
+  while i < length-1 
+    j = 0
+    while j < length-i-1 
+      if array[j].length > array[j+1].length # compares the length of each word
+        temp = array[j]
+        array[j] = array[j+1]
+        array[j+1] = temp
+      end
+      j += 1
+    end
+    i += 1
+  end
+
+  return array 
 end
