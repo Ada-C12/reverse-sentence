2755570cda1e34accef47ce7acf18973f5071153
dora1405 <li.dora05@gmail.com>
2019-09-22 13:18:47 -0700

    Wave 1 complete

--- lib/sort_by_length.rb
+++ lib/sort_by_length.rb
@@ -1,7 +1,23 @@
 # A method which will return an array of the words in the string
 #  sorted by the length of the word.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n^2)
+# Space complexity: O(n)
 def sort_by_length(my_sentence)
-  raise NotImplementedError, "Method not implemented"
+  word_array = my_sentence.split(" ")
+  i = 1
+  while i < word_array.length
+    to_insert = word_array[i]
+    j = i
+    while j > 0 && word_array[j-1].length > to_insert.length
+      word_array[j] = word_array[j-1]
+      j -= 1
+    end
+    word_array[j] = to_insert
+    i += 1
+  end
+  return word_array
+  
 end
+
+# my_sentence = ["dyson", "neutron", "star"]
+# p sort_by_length(my_sentence)
