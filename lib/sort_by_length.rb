3c3d960ec29ec3bfbec659e3231d5b78336e3854
Samantha Coll <samcoll@Samanthas-MacBook-Pro.local>
2019-09-22 16:10:52 -0700

    created method sort_by_length, tests passing

--- lib/sort_by_length.rb
+++ lib/sort_by_length.rb
@@ -1,7 +1,29 @@
 # A method which will return an array of the words in the string
 #  sorted by the length of the word.
-# Time complexity: ?
-# Space complexity: ?
+
+# Time complexity: O(n^2) - bubble sort - depends on the length of the array for two loops 
+# Space complexity: O(n) - creates a new array for the string in "my_sentence"
+
+# Using bubble sort
 def sort_by_length(my_sentence)
-  raise NotImplementedError, "Method not implemented"
+  array = my_sentence.split(" ")
+  length = array.length
+  
+  i = 0
+  # Outer loop
+  while i < length-1
+    j = 0
+    # Inner loop
+    while j < length-1
+      # Swap
+      if array[j].length > array[j+1].length
+        temp = array[j]
+        array[j] = array[j+1]
+        array[j+1] = temp
+      end
+      j += 1
+    end
+    i += 1
+  end
+  return array
 end
