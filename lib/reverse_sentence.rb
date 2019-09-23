ecb9c885e39f4968691a80de239162504be916ee
Natalie Tapias <nataliemtapias@gmail.com>
2019-09-23 09:37:23 -0700

    It is not producing th desired result but I am learning something.

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -6,5 +6,17 @@ def reverse_sentence(my_sentence)
     return my_sentence
   end
   
+  
+  i = 0
+  j = (my_sentence.length - 1)
+  
+  until i == my_sentence.length
+    holding = my_sentence[j]
+    my_sentence[i] = holding
+    i += 1
+    j -= 1
+  end
+  
+  return my_sentence
   raise NotImplementedError
 end
