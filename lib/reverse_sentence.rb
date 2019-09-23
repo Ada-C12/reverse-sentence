be8c844d810df6520c515bb5edf59d05c31141a3
Kelsey Krippaehne <kelseykripp@gmail.com>
2019-09-22 23:50:48 -0700

    Implemented working code for reverse_sentence and sort_by_length.

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -1,6 +1,53 @@
 # A method to reverse the words in a sentence, in place.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n^2), the first `while` loop in `reverse_sentence` will run 'n' times, and each time it calls the `insertion_sort` method that method could run 'n' times. 
+# Space complexity: O(n), where 'n' is the length of `my_sentence`.
+
+
+# helper method to shift each element over once place
+# I believe this is an insertion sort
+def insertion_sort(first_index, my_sentence, last_position, last_character)
+  while first_index < last_position
+    my_sentence[first_index] = my_sentence[first_index + 1]
+    first_index += 1
+  end
+  my_sentence[last_position] = last_character
+end
+
 def reverse_sentence(my_sentence)
-  raise NotImplementedError
+  if !my_sentence.nil?
+    word_length = 0
+    last_position = my_sentence.length - 1
+    while word_length < last_position
+      if my_sentence[word_length].match(/\s/)
+        
+        # store space character
+        space = my_sentence[word_length]
+        
+        # insert word at end of string
+        current_position = word_length - 1
+        while current_position >= 0
+          temp = my_sentence[current_position]
+          
+          insertion_sort(current_position, my_sentence, last_position, temp)
+          
+          current_position -= 1
+          last_position -= 1
+        end
+        
+        # insert space in front of word
+        insertion_sort(0, my_sentence, last_position, space)
+        
+        last_position -= 1
+        
+        # reset base index
+        word_length = 0
+        
+      else
+        # increment base index
+        word_length += 1
+      end
+    end
+  else
+    return my_sentence
+  end
 end
--- lib/sort_by_length.rb
+++ lib/sort_by_length.rb
@@ -1,7 +1,37 @@
 # A method which will return an array of the words in the string
 #  sorted by the length of the word.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n), the first `while` loop runs 'n' times, where 'n' is the length of `my_sentence`. The second `while` loop that contains the bubble sort runs 'n' times where 'n' is the length of the `full_words` array. This is maybe O(2n), or O(m + n), but since we ignore the constants it turns into O(n).
+# Space complexity: O(n), where 'n' is the length of `my_sentence`, even though one array is being made it's not significant enough to increase the space complexity. 
+
 def sort_by_length(my_sentence)
-  raise NotImplementedError, "Method not implemented"
+  length = my_sentence.length - 1
+  full_words = []
+  letter_index = 0
+  if !my_sentence.nil?
+    # runs `n` times
+    while letter_index <= length
+      word = ""
+      while my_sentence[letter_index] != " " && letter_index <= length
+        word += my_sentence[letter_index]
+        letter_index += 1
+      end
+      # adds a full word to `full_words` array
+      if word != " " && word != ""
+        full_words << word
+      end
+      letter_index += 1
+    end
+  end
+  i = 0
+
+  while i < (full_words.length - 1)
+    # bubble sort
+    if full_words[i].length > full_words[i + 1].length
+      temp = full_words[i]
+      full_words[i] = full_words[i + 1]
+      full_words[i + 1] = temp
+    end
+    i += 1
+  end
+  return full_words
 end
