8450790fa15d277c628e160e5fefa703b88336c6
dora1405 <li.dora05@gmail.com>
2019-09-22 21:32:54 -0700

    Wave 2, added comments for Time and Space complexity. Still issue with raking tests, submitting regardless. Will try to correct if possible, if not, then it's submitted as is.

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -1,6 +1,6 @@
 # A method to reverse the words in a sentence, in place.
-# Time complexity: O(n)
-# Space complexity: O(1)
+# Time complexity: O(n) - the n is because the number of times the loop runs is dependant on the length of my_sentence.
+# Space complexity: O(1) - no additional memory/space is used. The variable, my_sentence, starts as a string, then transform to an array, the loop runs (or doesn't if nil) and the variable changed back to a string per the .join method.
 def reverse_sentence(my_sentence)
 
   if my_sentence.nil?
@@ -21,5 +21,5 @@ def reverse_sentence(my_sentence)
 end
 
 
-my_sentence = "How  do  you   like     them      apples?"
-p reverse_sentence(my_sentence)
+# my_sentence = "How  do  you   like     them      apples?"
+# p reverse_sentence(my_sentence)
