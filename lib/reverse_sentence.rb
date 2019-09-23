d0bd7febb38dc8c0d4101c0478d60e05820ccead
Monick <monick.keo@gmail.com>
2019-09-22 20:47:13 -0700

    changed tests from test_string.must_equal to reverse_sentence(test_string).must_equal. Completed reverse sting tests

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -2,7 +2,23 @@
 # Time complexity: ?
 # Space complexity: ?
 def reverse_sentence(my_sentence)
-  raise NotImplementedError
+  if my_sentence == nil 
+    return my_sentence
+  end 
 
+  my_sentence += "*"
+  my_sentence = my_sentence.split(/\s+" "+|\s/)
+  i = 0
+  j = my_sentence.length - 1
 
+  while i < j
+    temp = my_sentence[i]
+    my_sentence[i] = my_sentence[j]
+    my_sentence[j] = temp
+
+    i += 1
+    j -= 1
+  end
+  my_sentence = my_sentence.join(" ").delete("*")
+  return my_sentence
 end
