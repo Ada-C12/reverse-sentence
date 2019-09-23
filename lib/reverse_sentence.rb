a7d6d43245b3e6717884ecb4ba2f8ebfafdec5f0
Janice Huang <janicehuang@gmail.com>
2019-09-22 17:11:09 -0700

    get to all working, all tests passing. still need refactoring

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -7,33 +7,50 @@ def reverse_sentence(my_sentence)
   elsif my_sentence == nil
     return nil
   end
-  length = my_sentence.length
-  swap(my_sentence, length)
-  
-  head_index = 0
-  tail_index = 0
-  while tail_index < length
-    if my_sentence[head-index] == " "
-      head_index += 1
-      tail_index += 1
-    else until tail_index == " "
-      tail_index += 1
-    end
-    swap(my_sentence.slice(head_index..tail_index), (tail_index - head_index + 1))
-  end
-  return my_sentence
-end
 
-def swap(to_swap, length)
+  length = my_sentence.length
   front_index = 0
-  num_swaps = length / 2
   back_index = length - 1
-  num_swaps.times do
-    to_swap[front_index], to_swap[back_index] = to_swap[back_index], to_swap[front_index]
+  num_swap = length / 2
+  num_swap.times do
+    my_sentence[front_index], my_sentence[back_index] = my_sentence[back_index], my_sentence[front_index]
     front_index += 1
     back_index -= 1
   end
-  return to_swap
+  p my_sentence
+  head_index = 0
+  tail_index = 1
+  while tail_index < length
+    if my_sentence[head_index] == " "
+      head_index += 1
+      tail_index += 1
+    elsif my_sentence[head_index]!= " " && my_sentence[tail_index] != " " && tail_index < (length - 1)
+      tail_index += 1
+    elsif my_sentence[head_index] != " " && my_sentence[tail_index] == " " && (tail_index - head_index) == 1
+      head_index += 1
+      tail_index += 1
+    elsif my_sentence[head_index]!= " " && my_sentence[tail_index] != " " && tail_index == (length - 1)
+      inside_length = tail_index - head_index + 1
+      front_index = head_index
+      back_index = tail_index
+      (inside_length / 2).times do
+        my_sentence[front_index], my_sentence[back_index] = my_sentence[back_index], my_sentence[front_index]
+        front_index += 1
+        back_index -= 1
+      end
+      tail_index += 1
+    elsif (my_sentence[head_index] != " " && my_sentence[tail_index] == " " && (tail_index - head_index) > 1)
+      inside_length = tail_index - head_index + 1
+      front_index = head_index
+      back_index = tail_index - 1
+      (inside_length / 2).times do
+        my_sentence[front_index], my_sentence[back_index] = my_sentence[back_index], my_sentence[front_index]
+        front_index += 1
+        back_index -= 1
+      end
+      head_index = tail_index
+      tail_index += 1
+    end
+  end
+  return my_sentence
 end
-
-p reverse_sentence("build a snowman")
