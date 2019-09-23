b3ca62881abba794294383d45a8b4ab253c86d2b
Alice <alicesunhi@gmail.com>
2019-09-23 09:25:44 -0700

    sorting string manipulation

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -1,6 +1,38 @@
 # A method to reverse the words in a sentence, in place.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n^2), linear, because there are three while loops within reverse_sentence() and reverse(), and two that are nested. The amount of iterations may increase proportionately to the data being passed through.
+# Space complexity: O(1), constant, because the only data that is being stored is the start/ending/temp variables, and the array being passed through is being reversed in place.
 def reverse_sentence(my_sentence)
-  raise NotImplementedError
+  return [] if my_sentence.nil? || my_sentence.length == 0
+  
+  reverse(my_sentence, 0, my_sentence.length-1)
+  
+  start = 0
+  
+  while start < my_sentence.length
+    if my_sentence[start] == " "
+      start+=1
+    else
+      ending = start
+      
+      while ending < my_sentence.length && my_sentence[ending] != " "
+        ending+=1
+      end
+      
+      reverse(my_sentence, start, ending-1)
+      
+      start = ending
+    end
+  end
+end
+
+
+def reverse(string, i, j)
+  while i < j
+    temp = string[i]
+    string[i] = string[j]
+    string[j] = temp
+    i+=1
+    j-=1
+  end
+  
 end
--- lib/sort_by_length.rb
+++ lib/sort_by_length.rb
@@ -1,7 +1,26 @@
 # A method which will return an array of the words in the string
 #  sorted by the length of the word.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n^2), quadratic, because there are two while loops in the insertion sort function that will both iterate n times.
+# Space complexity: O(n), linear, because the new array that is being stored will increase depending on the size of the data (my_sentence) being passed through.
 def sort_by_length(my_sentence)
-  raise NotImplementedError, "Method not implemented"
+  return [] if my_sentence.nil? || my_sentence.length == 0
+  
+  return insertion_sort(my_sentence.split(" "))
+end
+
+def insertion_sort(array)
+  length = array.length
+  
+  i = 1
+  while i < length
+    to_insert = array[i]
+    j = i
+    while j > 0 && array[j - 1].length > to_insert.length
+      array[j] = array[j-1]
+      j-=1
+    end
+    array[j] = to_insert
+    i+=1
+  end
+  return array
 end
