9b7a9ac55b86375eaac5760a8a5dde43158fa349
Nicky Choi <32828004+njch5@users.noreply.github.com>
2019-09-22 22:21:06 -0700

    Update sort_by_length.rb

--- lib/sort_by_length.rb
+++ lib/sort_by_length.rb
@@ -1,7 +1,7 @@
 # A method which will return an array of the words in the string
 #  sorted by the length of the word.
 # Time complexity: O(n^2) n is the split_sentence array which gets iterated over until there are no more elements left
-# Space complexity: O(n) n is for the empty sorted array which takes up additional space
+# Space complexity: O(n) n is for the sorted array which takes up additional space
 def sort_by_length(my_sentence)
   sorted = []
   split_sentence = my_sentence.split(" ")
