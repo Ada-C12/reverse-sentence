e96a21b25897d73352d8e972201f9084c7740aa5
Janice Huang <janicehuang@gmail.com>
2019-09-22 20:50:51 -0700

    refactor

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -1,55 +1,49 @@
 # A method to reverse the words in a sentence, in place.
-# Time complexity: O(n) The (very ugly and repetitive code in this) program has to run through n twice--the first time to reverse all of the characters, and the second time to re-reverse (correct) each individual word. The constant '2' drops away.
-# Space complexity: O(1)? Because the string is modifying in place, it doesn't take up additional space. 
+# Time complexity: O(n) The program has to run through n thrice--the first time to calculate the length of the string, the second time to reverse all of the characters, and the third time to re-reverse (correct) each individual word. The constant '3' drops away.
+# Space complexity: O(1) Because the string is modifying in place, it doesn't take up additional space.
 def reverse_sentence(my_sentence)
+  # edge cases
   if my_sentence == ""
     return ""
   elsif my_sentence == nil
     return nil
   end
 
+  # first reverse all characters
+  switcheroo(my_sentence)
+  # now go back and switch back individual words
+  front_index = 0
+  back_index = 1
   length = my_sentence.length
+  while back_index <= length
+    # bypass blank spaces and single-letter words
+    if my_sentence[front_index] == " " || my_sentence[front_index] != " " && my_sentence[back_index] == " " && back_index - front_index == 1
+      front_index += 1
+      back_index += 1
+
+      # for end of sentence or word
+    elsif my_sentence[front_index] != " " && my_sentence[back_index] != " " && (my_sentence[back_index + 1] == nil || my_sentence[back_index + 1] == " ")
+      my_sentence.sub!(my_sentence[front_index..back_index],switcheroo(my_sentence[front_index..back_index]))
+      front_index = back_index
+      back_index = front_index + 1
+      
+      # if front is char, move back index to end of word
+    elsif my_sentence[front_index]!= " " && my_sentence[back_index] != " "
+      back_index += 1
+    end
+  end
+  return my_sentence
+end
+
+def switcheroo(string)
+  length = string.length
   front_index = 0
   back_index = length - 1
   num_swap = length / 2
   num_swap.times do
-    my_sentence[front_index], my_sentence[back_index] = my_sentence[back_index], my_sentence[front_index]
+    string[front_index], string[back_index] = string[back_index], string[front_index]
     front_index += 1
     back_index -= 1
   end
-  head_index = 0
-  tail_index = 1
-  while tail_index < length
-    if my_sentence[head_index] == " "
-      head_index += 1
-      tail_index += 1
-    elsif my_sentence[head_index]!= " " && my_sentence[tail_index] != " " && tail_index < (length - 1)
-      tail_index += 1
-    elsif my_sentence[head_index] != " " && my_sentence[tail_index] == " " && (tail_index - head_index) == 1
-      head_index += 1
-      tail_index += 1
-    elsif my_sentence[head_index]!= " " && my_sentence[tail_index] != " " && tail_index == (length - 1)
-      inside_length = tail_index - head_index + 1
-      front_index = head_index
-      back_index = tail_index
-      (inside_length / 2).times do
-        my_sentence[front_index], my_sentence[back_index] = my_sentence[back_index], my_sentence[front_index]
-        front_index += 1
-        back_index -= 1
-      end
-      tail_index += 1
-    elsif (my_sentence[head_index] != " " && my_sentence[tail_index] == " " && (tail_index - head_index) > 1)
-      inside_length = tail_index - head_index + 1
-      front_index = head_index
-      back_index = tail_index - 1
-      (inside_length / 2).times do
-        my_sentence[front_index], my_sentence[back_index] = my_sentence[back_index], my_sentence[front_index]
-        front_index += 1
-        back_index -= 1
-      end
-      head_index = tail_index
-      tail_index += 1
-    end
-  end
-  return my_sentence
+  return string
 end
