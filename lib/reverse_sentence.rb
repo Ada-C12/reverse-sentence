d9c85e48c153c48ca89c16c1417b0e73e7522fb5
Natalie Tapias <nataliemtapias@gmail.com>
2019-09-23 09:41:55 -0700

    I've now reversed all characters and am thinking about how to swap each word in place.

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -9,12 +9,19 @@ def reverse_sentence(my_sentence)
   
   i = 0
   j = (my_sentence.length - 1)
+  punctuation = [',', '.', '!', ':', ';', '?']
   
+  # reverses each character
   until i == my_sentence.length
     holding = my_sentence[j]
     my_sentence[i] = holding
     i += 1
     j -= 1
+  end
+  
+  # swap each character of each word back into space
+  my_sentence 
+  until 
   end
   
   return my_sentence
