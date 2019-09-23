63c192d7c5b42bdb63130fe086f7885406cca396
dora1405 <li.dora05@gmail.com>
2019-09-22 21:26:01 -0700

    Wave 2, working code if run from reverse_sentence.rb but not if raking per the tests

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -1,6 +1,25 @@
 # A method to reverse the words in a sentence, in place.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n)
+# Space complexity: O(1)
 def reverse_sentence(my_sentence)
-  raise NotImplementedError
+
+  if my_sentence.nil?
+    return nil
+  else
+    my_sentence = my_sentence.split(/\s/)
+    i = 0
+    last = -1
+    (my_sentence.length/2).times do |i|
+      temp = my_sentence[i]
+      my_sentence[i] = my_sentence[last]
+      my_sentence[last] = temp
+      last -= 1
+    end
+  end
+  my_sentence = my_sentence.join(" ")
+  return my_sentence
 end
+
+
+my_sentence = "How  do  you   like     them      apples?"
+p reverse_sentence(my_sentence)
