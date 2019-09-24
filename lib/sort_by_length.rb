b42f05f46dea9e81abf744d97d57f817b7c6139e
Emily Vomacka <emilyvomacka@gmail.com>
2019-09-23 17:10:11 -0700

    added selection sort to sort_by_length, all tests passing

--- lib/sort_by_length.rb
+++ lib/sort_by_length.rb
@@ -3,5 +3,24 @@
 # Time complexity: ?
 # Space complexity: ?
 def sort_by_length(my_sentence)
-  raise NotImplementedError, "Method not implemented"
+  sentence_array = my_sentence.split
+  length = sentence_array.length 
+  (0...(length - 1)).each do |i|
+    min_index = i
+    j = i + 1
+    while j < length
+      if sentence_array[j].length < sentence_array[min_index].length
+        min_index = j
+      end
+      j += 1
+    end
+    if min_index != i
+      temp = sentence_array[min_index]
+      sentence_array[min_index] = sentence_array[i]
+      sentence_array[i] = temp
+    end
+    i += 1
+  end
+  return sentence_array
 end
+
