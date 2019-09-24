06c3d2ef2397a39065dfe9fe856030b19b2f00b0
Raisah Vesteinsdottir <rvesteinsdottir@gmail.com>
2019-09-24 15:05:25 -0700

    Completed sort_by_length method

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -2,5 +2,100 @@
 # Time complexity: ?
 # Space complexity: ?
 def reverse_sentence(my_sentence)
-  raise NotImplementedError
+  i = 0
+  j = my_sentence.length - 1
+  
+  while i < (my_sentence.length / 2)
+    temp_a = my_sentence[i]
+    temp_b = my_sentence[j]
+    
+    my_sentence[i] = temp_b
+    my_sentence[j] = temp_a
+    
+    i += 1 
+    j -= 1
+  end
+  
+  x = 0
+  word_start = 0
+  word_end = 0
+  in_word = true
+  
+  while x < my_sentence.length
+    if (my_sentence[x] == " " || x == (my_sentence.length - 1) )&& in_word == true
+      if my_sentence[x] == " "
+        word_end = x - 1
+      elsif x == (my_sentence.length - 1)
+        word_end = x
+      end
+      
+      word_length = (word_end - word_start + 1)
+      puts word_length
+      
+      y = word_start
+      z = word_end
+      count = 0
+      
+      
+      while count < (word_length / 2)
+        temp_char_one = my_sentence[y]
+        temp_char_two = my_sentence[z]
+        
+        my_sentence[y] = temp_char_two
+        my_sentence[z] = temp_char_one
+        
+        y += 1
+        z -= 1
+        count += 1
+      end
+      
+      in_word = false
+    end
+    
+    if my_sentence[x] != " " && in_word == false
+      word_start = x
+      in_word = true
+    end
+    
+    x += 1
+  end
+  
+  
+  # until my_sentence[i] == " "
+  #   i += 1
+  # end
+  # first_word = my_sentence[0, i]
+  
+  
+  # j = my_sentence.length - 1
+  # until my_sentence[j] == " "
+  #   j -= 1
+  # end
+  # last_word = my_sentence[j + 1, my_sentence.length - 1]
+  
+  # my_sentence.insert(0, my_sentence[j + 1, my_sentence.length - 1])
+  # my_sentence.delete!("better")
+  
+  puts word_start
+  puts word_end
+  
+  return my_sentence
 end
+
+puts reverse_sentence("I'm an   animal")
+
+
+
+# words = []
+# start = 0
+
+# while i < my_sentence.length
+#   if my_sentence[i] == " " || i == my_sentence.length - 1
+#     finish = i
+#     words << my_sentence[start, finish]
+#     start = i
+#   end
+
+#   i += 1
+
+# end
--- lib/sort_by_length.rb
+++ lib/sort_by_length.rb
@@ -3,5 +3,28 @@
 # Time complexity: ?
 # Space complexity: ?
 def sort_by_length(my_sentence)
-  raise NotImplementedError, "Method not implemented"
+  my_sentence_array = my_sentence.split(" ")
+  
+  i = 1
+  while i < my_sentence_array.length
+    value = i
+    j = i - 1
+    i.times do
+      if my_sentence_array[value].length < my_sentence_array[j].length
+        new_value = my_sentence_array[j]
+        new_j = my_sentence_array[value]
+        
+        my_sentence_array[value] = new_value
+        my_sentence_array[j] = new_j
+      end
+      j -= 1
+      value -= 1
+    end
+    i += 1
+  end
+  
+  return my_sentence_array
 end
+
+
+puts sort_by_length("words are the a")
