7d7f7e61fd92d74d9028f328246bf7b2be0ef9bf
Brianna Kemp <kemp.bri@gmail.com>
2019-09-27 22:56:31 -0700

    typo in space complexity corrected

--- lib/sort_by_length.rb
+++ lib/sort_by_length.rb
@@ -1,7 +1,7 @@
 # A method which will return an array of the words in the string
 #  sorted by the length of the word.
 # Time complexity: n^2
-# Space complexity: O(1=n)
+# Space complexity: O(n)
 def sort_by_length(my_sentence)
   return nil if my_sentence.nil?
   
