ae3fd8131c88bf2412dbba90384ed698b7c8fb5f
Brianna Kemp <kemp.bri@gmail.com>
2019-09-27 23:33:15 -0700

    Wave 2 version implemented, tests not passing

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -2,5 +2,19 @@
 # Time complexity: ?
 # Space complexity: ?
 def reverse_sentence(my_sentence)
-  raise NotImplementedError
+  return nil if my_sentence.nil?
+  i = 0
+  my_array = my_sentence.split
+  length = my_array.length
+  
+  while i < my_array.length/2 do
+    temp = my_array[i]
+    my_array[i] = my_array[length - 1 - i]
+    my_array[length - 1 - i] = temp
+    i += 1
+  end
+  my_sentence = my_array.join(" ")
+  return my_sentence
 end
+
+p reverse_sentence("Yoda is awesome!")
