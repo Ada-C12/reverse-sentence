28e3bfa1d3fe845aa9fedaf1467a8f3a67e6881c
Brianna Kemp <kemp.bri@gmail.com>
2019-09-27 22:54:28 -0700

    Initial wave 1 implemented, still needs refactoring/error handling

--- lib/sort_by_length.rb
+++ lib/sort_by_length.rb
@@ -1,7 +1,20 @@
 # A method which will return an array of the words in the string
 #  sorted by the length of the word.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: n^2
+# Space complexity: O(1=n)
 def sort_by_length(my_sentence)
-  raise NotImplementedError, "Method not implemented"
+  my_sorted_array = []
+  my_array = my_sentence.split
+  
+  until my_array.length == 0 do
+    current_word = my_array[0]
+    my_array.each do |word|
+      if word.length  < current_word.length
+        current_word = word
+      end
+    end
+    my_sorted_array.push(current_word)
+    my_array.delete(current_word)
+  end
+  return my_sorted_array
 end
