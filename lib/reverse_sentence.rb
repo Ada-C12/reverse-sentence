fab0cfb1a5d8f3d7b81b025f203240a41fedc1bf
Yasmin <yasmin312002@hotmail.com>
2019-09-29 16:43:05 -0700

    wave 1 all tests passes, wave 2 all tests passed except 2 failures(spaces and white spaces)

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -1,6 +1,31 @@
+#require "pry"
 # A method to reverse the words in a sentence, in place.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: o(n)
+# Space complexity: o(1)
 def reverse_sentence(my_sentence)
-  raise NotImplementedError
+  #raise NotImplementedError
+  if my_sentence == nil 
+    return my_sentence
+  end
+  words = my_sentence.split() 
+  i = 0
+  j = words.length - 1
+  
+  while i < j do 
+    temp = words[i]
+    # binding.pry
+    words[i] = words[j]
+    words[j] = temp
+    # binding.pry
+    i += 1
+    j -= 1
+  end 
+  # binding.pry
+  # print words
+   final_word = words.join(" ")
+  return final_word
 end
+
+
+
+
--- lib/sort_by_length.rb
+++ lib/sort_by_length.rb
@@ -1,7 +1,19 @@
 # A method which will return an array of the words in the string
 #  sorted by the length of the word.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: o(n^2)
+# Space complexity: o(1)
 def sort_by_length(my_sentence)
-  raise NotImplementedError, "Method not implemented"
+  #raise NotImplementedError, "Method not implemented"
+  new_sentence = my_sentence.split()
+  n = new_sentence.length
+  for j in 0..n-1
+    for i in 0..n-2-j
+      if new_sentence[i].length > new_sentence[i + 1].length
+        temp = new_sentence[i]
+        new_sentence[i] = new_sentence[i+1]
+        new_sentence[i + 1] = temp
+      end
+    end
+  end
+  return new_sentence
 end
