cefb578ba9f82b05a8c8a4c25b87981fcd22e905
Rinostar <codingrinostar@gmail.com>
2019-09-28 21:07:29 -0700

    wave_2 finalized

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -3,16 +3,13 @@
 # Space complexity: O(n)
 def reverse_sentence(my_sentence) 
   #Step_0: Screen out all sentences that doesn't need to be/can't be reversed
-  if my_sentence == nil || my_sentence == ""
-    return my_sentence
-    exit
-  elsif my_sentence.include?("\s") == false
+  if my_sentence == nil || my_sentence == "" || my_sentence.include?("\s") == false
     return my_sentence
     exit
   end
 
   #Step_1: Reverse all characters within the string in place
-  sentence_pairs = paris_helper(my_sentence.length)
+  sentence_pairs = pairs_helper(my_sentence.length)
   i = 1
   (sentence_pairs).times do
     temp = my_sentence[i-1]
@@ -21,35 +18,12 @@ def reverse_sentence(my_sentence)
     i += 1
   end
 
-  #Step_2: Find all indexes for the whitespaces within the current string
+  #Step_2: Find all whitespace index(es) within the current string
   space_indexes = (0...my_sentence.length).find_all { |c| my_sentence[c] == " " }
 
-  #Step_3: Reverse all characters for each word within the string in place
-  if space_indexes.length == 1
-    my_sentence.reverse_first
-    my_sentence.reverse_last
-  else
-    my_sentence.reverse_first
-    my_sentence.reverse_last
-    my_sentence.reverse_middle
-  end
-
-  return my_sentence
-end
-
-# Helper methoder to determin pairs of character that needs to be swapped
-def paris_helper(length)
-  if my_sentence.length % 2 == 0
-    pairs = my_sentence.length / 2
-  else
-    pairs = (my_sentence.length-1) / 2
-  end
-  return pairs
-end
-
-# Helpfer method to reverse the first word in the string
-def reverse_first
-  word_pairs = paris_helper(space_indexes[0])
+  #Step_3: Reverse all characters for each word based on the whitespace index(es)
+  #Step_3.1: Reverse the first word in the string
+  word_pairs = pairs_helper(space_indexes[0])
   i = 1
   (word_pairs).times do
     temp = my_sentence[i-1]
@@ -57,11 +31,8 @@ def reverse_first
     my_sentence[space_indexes[0]-i] = temp
     i += 1
   end
-end
-
-# Helpfer method  to reverse the last word in the string
-def reverse_last
-  word_pairs = paris_helper(space_indexes[-1])
+  #Step_3.2: reverse the last word in the string
+  word_pairs = pairs_helper((my_sentence.length - 1) - space_indexes[-1])
   i = 1
   (word_pairs).times do
     temp = my_sentence[-i]
@@ -69,23 +40,37 @@ def reverse_last
     my_sentence[space_indexes[-1]+i] = temp
     i += 1
   end
-end
-
-# Helper method to reverse the middle parts of the string (word(s) between the first and last words)
-def reverse_middle
-  until tracker == space_indexes[-1]
-    i = 0
-    tracker == space_indexes[i]
-    if my_sentence[space_indexes[i]+1] != " "
-      word_pairs = paris_helper(space_indexes[i+1]-space_indexes[i]-1)
-      n = 1
-      (word_pairs).times do
-        temp = my_sentence[space_indexes[i]+n]
-        my_sentence[space_indexes[i]+n] = my_sentence[space_indexes[i+1]-n]
-        my_sentence[space_indexes[i+1]-n] = temp
-        n += 1
+  #Step_3.3: reverse the words in between (if any)
+  i = 0
+  if space_indexes.length == 1
+    return my_sentence
+  else
+    until i == (space_indexes.length - 1)
+      if my_sentence[space_indexes[i]+1] != " "
+        word_pairs = pairs_helper(space_indexes[i+1]-space_indexes[i]-1)
+        n = 1
+        (word_pairs).times do
+          temp = my_sentence[space_indexes[i]+n]
+          my_sentence[space_indexes[i]+n] = my_sentence[space_indexes[i+1]-n]
+          my_sentence[space_indexes[i+1]-n] = temp
+          n += 1
+        end
       end
+      i += 1
     end
-    i += 1
+    return my_sentence
   end
 end
+
+# Helper methoder to determin pairs of character that needs to be swapped
+def pairs_helper(length)
+  if length % 2 == 0
+    pairs = length / 2
+  else
+    pairs = (length - 1) / 2
+  end
+  return pairs
+end
+
+# test_string = "Yoda is awesome!"
+# test_result = reverse_sentence(test_string)
