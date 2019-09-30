7cfcce5c3f43d0d8ec4bb670888c70d0b56fcbfe
ohcloud <cloudylopez@icloud.com>
2019-09-29 20:27:52 -0700

    sort by length done. reverse sort is not.

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -1,6 +1,24 @@
 # A method to reverse the words in a sentence, in place.
 # Time complexity: ?
 # Space complexity: ?
+
+# min = my_sentence[0]
+# max = my_sentence[-1]
+# my_arr = #["yoda"__"is"___"awesome"].dup and add in spaces to new array[?]
+# my_arr = ["yoda", "__", "is", "___", "awesome"]
+# my_new_arr = []
+# loop thru 
+# first = my_arr[0] 
+# last = my_arr[-1]
+# first, last = last, first 
+# last += 1
+# first -= -1
+# shovel into my_new_array 
+# in a perfect world, would return in order we want
+
 def reverse_sentence(my_sentence)
-  raise NotImplementedError
+  return nil if my_sentence == nil
+  my_sentence.split(" ")
+  
+  
 end
--- lib/sort_by_length.rb
+++ lib/sort_by_length.rb
@@ -2,6 +2,34 @@
 #  sorted by the length of the word.
 # Time complexity: ?
 # Space complexity: ?
+
+
+# Sky_array =[ The sky is blue ].split (“ “)
+# => [“the”, “sky”, “is”, “blue"]
+# sky_array =  3  3  2  4#character count
+# find min, then find max
+#range loop, min to max, 
+# Output = [ is the sky blue ]
+
+sky_arr = "the sky is blue"
 def sort_by_length(my_sentence)
-  raise NotImplementedError, "Method not implemented"
+  return nil if my_sentence == nil
+  final_arr = []
+  sky_arr = my_sentence.split(" ")
+  length_arr = sky_arr.map do |word|
+    word.length
+  end
+  
+  min = length_arr.min 
+  max = length_arr.max
+  
+  (min..max).each do |length|
+    sky_arr.each do |word|
+      if word.length == length
+        final_arr << word
+      end
+    end
+  end
+  return final_arr
 end
+puts sort_by_length(sky_arr)
