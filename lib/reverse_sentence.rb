e48f7dd14468821ef79c075ce6f09abf102afbbd
brilatimer <brihiramine@gmail.com>
2019-09-29 20:26:51 -0700

    completed wave 2

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -1,9 +1,9 @@
 # A method to reverse the words in a sentence, in place.
 # Time complexity: ?
 # Space complexity: ?
-
+require 'pry'
 # reverse string given start and ending index
-def reverse(string, start, ending)
+def reverse_word(string, start, ending)
   
   i = start
   j = ending
@@ -11,11 +11,38 @@ def reverse(string, start, ending)
   while i < j
     temp = string[i]
     string[i] = string[j]
-    string[j] = string[i]
+    string[j] = temp
     
     i += 1
     j -= 1
   end
 end
 
+# call method made to reverse sentence
 
+def reverse_sentence(string)
+  if string == nil
+    return
+  end
+  reverse_word(string, 0, string.length - 1)
+  start = 0
+  while start < string.length
+    if string[start] == " "
+      start += 1 
+      next
+      
+    else # found the start of a word
+      ending = start
+      while ending <= string.length
+        if string[ending] == " " || ending == string.length
+          reverse_word(string, start, ending - 1)
+          start = ending
+          break
+        end
+        
+        ending += 1 
+        
+      end
+    end
+  end
+end
--- lib/sort_by_length.rb
+++ lib/sort_by_length.rb
@@ -2,15 +2,17 @@
 #  sorted by the length of the word.
 # Time complexity: O(N^2)
 # Space complexity: O(N)
+require "pry"
 def sort_by_length(my_sentence)
   sentence_array = my_sentence.split(" ")
-  return bubble_sort(sentence_array, sentence_array.length)
+  bubble_sort(sentence_array, sentence_array.length)
+  return sentence_array
 end
 
 # cite: https://github.com/Ada-Developers-Academy/textbook-curriculum/blob/master/04-cs-fundamentals/classroom/Sorting.md
 def bubble_sort(array, length)
   i = 0
-  while i < length-1 # outer loop
+  while i < length - 1 # outer loop
     j = 0
     while j < length-i-1 # inner loop
       if array[j].length > array[j+1].length # swap if length of word is greater than other
