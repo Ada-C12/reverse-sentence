7420c591dea8dc9f2bb40b6d6bec63a4777cfe13
brilatimer <brihiramine@gmail.com>
2019-09-29 19:52:02 -0700

    solved not in place whoops

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -1,6 +1,16 @@
 # A method to reverse the words in a sentence, in place.
 # Time complexity: ?
 # Space complexity: ?
+require 'pry'
+
+
 def reverse_sentence(my_sentence)
-  raise NotImplementedError
+  reverse_array = []
+  words = my_sentence.split(" ")
+  words.length.times do |index|    
+    last_index = words.length - 1 - index
+    reverse_array << words[last_index]
+  end
+  # binding.pry
+  return reverse_array.join(" ")
 end
