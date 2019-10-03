405704727e43929be4c52bba1c321f6428b023b0
stupendousC <CarolineWuKaplan@gmail.com>
2019-10-02 23:03:58 -0700

    passed tests, reverse_sentence actually does its job IN PLACE this time lol

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -1,37 +1,93 @@
-# A method to reverse the words in a sentence, in place.
-# Time complexity: O(n) overall.  I think Str[0...-1] is O(k) where k is based on avg word size and therefore constant
-# Space complexity: O(n)
+# A method to reverse the words in a sentence, IN PLACE.
+# Time complexity: O(m * n)???
+# Space complexity: O(n), I just need a set of variables as temporary storage 
 
 def reverse_sentence(my_sentence)
-  return my_sentence unless my_sentence
+  return unless my_sentence
+  return if my_sentence == ""
   
   index = 0
-  new_array = []
+  count = 0
   curr_str = ""
+  buffer = " "
+  iron_curtain = -1
+  len = my_sentence.length
   
-  while my_sentence[index]      # O(n)
+  while count < len     # O(n)
+    # puts "Looking at index #{index}, which is #{my_sentence[index]}"
     char = my_sentence[index]
-    
-    if curr_str[-1] != " "
-      curr_str << char
-    elsif curr_str[-1] == " "
-      curr_str = curr_str[0...-1]   #O(k)?
-      new_array << curr_str
-      curr_str = char
-    end
-    
     index += 1
+    count += 1
+    
+    unless char == " "
+      curr_str << char
+      
+    else
+      # puts "curr_str is now #{curr_str} which is ready to move to end"
+      # first modify to delete curr_str out of my_sentence
+      clone = my_sentence[index..-1].clone
+      my_sentence.delete!(my_sentence)
+      my_sentence << clone
+      # puts "my_sentence got cut down to #{my_sentence}"
+      
+      # modify to add each letter back bit by bit
+      while curr_str[-1]
+        my_sentence.insert(iron_curtain, curr_str[-1])
+        iron_curtain -= 1
+        curr_str = curr_str[0...-1]
+        # puts "my_sentence added back up to #{my_sentence}"
+      end
+      
+      my_sentence.insert(iron_curtain, buffer)
+      # puts "adding space... my_sentence added back up to #{my_sentence}"
+      iron_curtain -= 1
+      
+      # reset index for next word in front of string
+      index = 0
+    end   
   end
-  new_array << curr_str
   
-  rev_array = []
-  new_array.each do |word|    # O(n)
-    rev_array.unshift word
-  end
-  
+  # puts "\n\nANSWER = #{my_sentence.inspect}"
+  clone = my_sentence.clone
   my_sentence.delete!(my_sentence)
-  my_sentence << rev_array.join(" ")    #O(n)
+  my_sentence << clone
+  
 end
+
+
+############################################################################################################
+# def reverse_sentence_NOT_IN_PLACE(my_sentence)
+#   # Time complexity: O(n) overall.  I think Str[0...-1] is O(k) where k is based on avg word size and therefore constant
+#   # Space complexity: O(n)
+#   return my_sentence unless my_sentence
+
+#   index = 0
+#   new_array = []
+#   curr_str = ""
+
+#   while my_sentence[index]      # O(n)
+#     char = my_sentence[index]
+
+#     if curr_str[-1] != " "
+#       curr_str << char
+#     elsif curr_str[-1] == " "
+#       curr_str = curr_str[0...-1]   #O(k)?
+#       new_array << curr_str
+#       curr_str = char
+#     end
+
+#     index += 1
+#   end
+#   new_array << curr_str
+
+#   rev_array = []
+#   new_array.each do |word|    # O(n)
+#     rev_array.unshift word
+#   end
+
+#   my_sentence.delete!(my_sentence)
+#   my_sentence << rev_array.join(" ")    #O(n)
+# end
 
 
 
