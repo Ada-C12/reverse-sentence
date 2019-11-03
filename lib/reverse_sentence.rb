2418f3209ecbb649f0b3792dd9a4f45862775499
KKennedyCodes <holmstedtk@gmail.com>
2019-11-03 13:22:56 -0800

    Reverse sentence completed with tests passing

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -1,33 +1,49 @@
-# # A method to reverse the words in a sentence, in place.
-# # Time complexity: ?
-# # Space complexity: ?
+def reverse_sentence(my_sentence)
+  if my_sentence == nil
+    return nil
+  end
+  my_sentence = string_reverse(my_sentence)
+  my_sentence = word_reverse(my_sentence)
+  return my_sentence
+end
 
+def string_reverse(string)
+  i = 0
+  j = string.length - 1
+  temp_i = ""
+  temp_j = ""
+  while i < j do
+    temp_i = string[i]
+    temp_j = string[j]
+    string[i] = temp_j
+    string[j] = temp_i
+    i+=1
+    j-=1
+  end
+  return string
+end
 
-# def reverse_sentence(my_sentence)
-#   reverse_sentence = my_sentence.split(" ")
-#   bubble_sort(reverse_sentence, reverse_sentence.length) 
-#   reversed = reverse_sentence.join(" ")
-#   return reversed
-# end
-
-# def bubble_sort(array, length)
-#   i = 0
-#   while i < length-1 # outer loop
-#     j = 0
-#     while j < length-i-1 # inner loop
-#       p array.index(array[j])
-#       p array.index(array[j+1])
-#       if array.index(array[j]) < array.index(array[j+1]) # swap
-#         temp = array[j]
-#         array[j] = array[j+1]
-#         array[j+1] = temp
-#       end
-#       j += 1
-#     end
-#     i += 1
-#   end
-# end 
-
-# test_string = "hello, world"
-
-# p reverse_sentence(test_string)
+def word_reverse(string)
+  i = 0
+  j = 0
+  length = string.length
+  while i < length
+    while string[j] != " " && j < length do
+      j += 1
+    end
+    j -= 1
+    word_start = i
+    word_end = j
+    while word_start < word_end do
+      temp_i = string[word_start]
+      temp_j = string[word_end]
+      string[word_start] = temp_j
+      string[word_end] = temp_i
+      word_start+=1
+      word_end-=1
+    end
+    i = j+2
+    j = i
+  end
+  return string
+end
