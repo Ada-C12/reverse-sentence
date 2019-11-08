733551332ae2662a24769b2b42e060672d8e906a
ohcloud <cloudylopez@icloud.com>
2019-11-07 21:17:51 -0800

    not working the way i want

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -1,10 +1,11 @@
 # A method to reverse the words in a sentence, in place.
+
 # Time complexity: ?
 # Space complexity: ?
 
 # min = my_sentence[0]
 # max = my_sentence[-1]
-# my_arr = #["yoda"__"is"___"awesome"].dup and add in spaces to new array[?]
+# my_arr = #["yoda"__"is"___"awesome!"].dup and add in spaces to new array[?]
 # my_arr = ["yoda", "__", "is", "___", "awesome"]
 # my_new_arr = []
 # loop thru 
@@ -16,9 +17,21 @@
 # shovel into my_new_array 
 # in a perfect world, would return in order we want
 
-def reverse_sentence(my_sentence)
-  return nil if my_sentence == nil
-  my_sentence.split(" ")
+def reverse(sentence)
+  initial = 0 
+  last = sentence.length - 1
   
-  
+  while initial < last
+    memo = sentence[initial]
+    sentence[initial] = sentence[last]
+    
+    sentence[last] = memo
+    
+    initial += 1
+    last -= 1
+    
+    return sentence 
+  end
 end
+
+puts reverse("y__i____c")
